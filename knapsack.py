4ac026f3bc3031e555131586937224a2f3a166f7
fongbb132 <fo_og@hotmail.com>
2017-07-15 01:13:41 -0500

    knapsack problem

--- knapsack.py
+++ knapsack.py
@@ -6,4 +6,23 @@ def knapsack(W, wt, val):
     else:
         return max(knapsack(W, wt[1:], val[1:]), (val[0] + knapsack(W-wt[0], wt[1:], val[1:])))
 
+def knapsack2(W, wt, val):
+    v = len(val)+1
+    
+    dp = [[0 for a in xrange(v)]for b in xrange(W+1)]
+
+    for w in xrange(W+1):
+        for j in xrange(v):
+            if w == 0 or j == 0:
+                dp[w][j] = 0 
+            # elif w >= wt[j-1]:
+            #     dp[w][j] = max(val[j-1] + dp[w-wt[j-1]][j-1], dp[w][j-1])
+            elif w<wt[j-1]:
+                dp[w][j] = dp[w][j-1]
+            else:
+                dp[w][j] = max(val[j-1] + dp[w-wt[j-1]][j-1], dp[w][j-1])
+            
+    return dp[W][v-1]
 print knapsack(50, [10, 20, 30], [60, 100, 120])
+
+print knapsack2(50, [10, 20, 30], [60, 100, 120])
