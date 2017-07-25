74e78a6d5712bca66c36c1417b30ac50469b8e3d
Ka Wing Fong <fongkawing@gmail.com>
2017-07-25 00:12:34 -0500

    correction for floyd marshall

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -9,11 +9,12 @@ def floyd_warshall(map):
     for i in xrange(l):
         for j in xrange(l):
             for k in xrange(l):
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+                dp[j][k] = min(dp[j][k], dp[j][i] + dp[i][k])
     return dp
 
 map = [[0, 5, 10**10, 10],[10**10, 0,  3, 10**10],[10**10, 10**10, 0, 1],[10**10, 10**10, 10**10, 0]]
-
-result = floyd_warshall(map)
-for a in result:
+map2 = [[0, 4, 6, 1],[10**10, 0, 2, 3],[10**10, 10**10, 0, 10**10],[10**10, 1, 4, 0]]
+# result = floyd_warshall(map)
+result2 = floyd_warshall(map2)
+for a in result2:
     print a
